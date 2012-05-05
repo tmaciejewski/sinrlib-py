cac6d03b8e570f684fdfdb6da55e7d53778a6609
Tomasz Maciejewski <ponton@jabster.pl>
2012-05-05 21:49:44 +0200

    raise NotLinked exception when the algorithm return not linked next node

--- sinrlib/simulation.py
+++ sinrlib/simulation.py
@@ -7,6 +7,9 @@ class Algorithm:
     def compute(self, uid, message, sender, links):
         return []
 
+class NotLinked(Exception):
+    pass
+
 class Simulation:
     def __init__(self, _model):
         self.model = _model
@@ -15,10 +18,12 @@ class Simulation:
         messages = []
         for node in self.model.nodes.keys():
             for receiver in algorithm.init(node, self.model.links[node]):
-                messages.append((node, receiver))
+                if receiver in self.model.links[node]:
+                    messages.append((node, receiver))
+                else:
+                    raise NotLinked("Node %d is not linked with %d" % (node, receiver))
 
-        print 'Starting', rounds, 'rounds with initial state:', \
-            messages
+        print 'Starting', rounds, 'rounds with initial state:', messages
 
         for i in range(rounds):
             sent, failed = self.model.eval(messages)
@@ -31,7 +36,13 @@ class Simulation:
             for s, r in sent:
                 try:
                     for receiver in algorithm.compute(r, True, s, self.model.links[r]):
-                        new_messages.append((r, receiver))
+                        if receiver in self.model.links[r]:
+                            new_messages.append((r, receiver))
+                        else:
+                            raise NotLinked("Node %d is not linked with %d" % (r, receiver))
+
+                except NotLinked:
+                    raise
 
                 except Exception as e:
                     print 'The algorithm for', r, 'raised an exception:', e.message
