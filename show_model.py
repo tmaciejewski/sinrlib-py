06ac96a5db238788dc5f741c45c647c73f021429
Tomasz Maciejewski <ponton@jabster.pl>
2012-06-12 18:58:24 +0200

    check show_model.py argument for model type

--- show_model.py
+++ show_model.py
@@ -2,7 +2,7 @@
 
 import sys, random
 
-sys.path.append('..')
+sys.path.append('sinrlib')
 
 from config import Config
 from models.gauss import GaussModel
@@ -12,19 +12,16 @@ from models.gadget import GadgetModel
 
 def main():
     config = Config()
-    config.range = 1
 
-    #model = GaussModel(config)
-    #model.generate(50, .85)
-
-    #model = UniformModel(config)
-    #model.generate(200, 8)
-
-    model = SocialModel(config)
-    model.generate(200, 10, 2, 0.1)
-
-    #model = GadgetModel(config)
-    #model.generate(5, 15, 0.1)
+    if sys.argv[1] == 'uniform':
+        model = UniformModel(config)
+        model.generate(200, 8)
+    elif sys.argv[1] == 'social':
+        model = SocialModel(config)
+        model.generate(200, 10, 2, 0.1)
+    elif sys.argv[1] == 'gadget':
+        model = GadgetModel(config)
+        model.generate(10, 5, 0.1)
 
     #for i in range(100):
     #    n = random.randint(30,200)
