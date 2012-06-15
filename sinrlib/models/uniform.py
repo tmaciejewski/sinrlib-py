2e015a690ec070cdb6f82022c29df99ebd460a11
Tomasz Maciejewski <ponton@jabster.pl>
2012-06-15 23:17:09 +0200

    optimize uniform model generation

--- sinrlib/models/uniform.py
+++ sinrlib/models/uniform.py
@@ -1,24 +1,26 @@
 import model, random
 
 class UniformModel(model.Model):
-    def generate(self, n, size, range_e):
-        for _ in range(100):
-            self.nodes = {}
-            self.links = {}
+    def generate(self, n, size, range_e = 0):
+        self.nodes = {}
+        self.links = {}
+        uid = 0
 
-            for uid in range(n):
-                x = random.random() * size
-                y = random.random() * size
+        while True:
+            x = random.random() * size
+            y = random.random() * size
 
-                self.nodes[uid] = model.Node(x, y)
-                self.links[uid] = []
+            node = model.Node(x, y)
+            self.links[uid] = []
 
-            for uid1, node1 in self.nodes.iteritems():
-                for uid2, node2 in self.nodes.iteritems():
-                    if uid1 != uid2 and node1 - node2 <= (1 - range_e) * self.config.range:
-                        self.links[uid1].append(uid2)
-            
-            if len(self.connected_components()) == 1:
-                return
+            for uid2, node2 in self.nodes.iteritems():
+                if node - node2 <= (1 - range_e) * self.config.range:
+                    self.links[uid].append(uid2)
+                    self.links[uid2].append(uid)
         
-        raise Exception("Can't generate connected graph")
+            self.nodes[uid] = node
+            uid += 1
+
+            if len(self.nodes) >= n:
+                if len(self.connected_components()) == 1:
+                    return
