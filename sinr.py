599e1e67ebb5bf54402e5b135932452e07a3e483
Tomasz Maciejewski <ponton@jabster.pl>
2012-07-25 07:58:01 +0200

    run simulation for different S

--- sinr.py
+++ sinr.py
@@ -9,52 +9,54 @@ def main():
     tries = int(sys.argv[1])
     start, end, step = [int(arg) for arg in sys.argv[2].split(',')]
 
-    C = 10
+    C = 1
     e = 0.1
-    S = 4
+    S = 2
+    d = 10
+    config.range = 1 - 6*e
 
     avgs = []
     stds = []
     ns  = []
 
     for N in range(start, end + 1, step):
-        results = []
-        while len(results) < tries:
-            try:
-                #model = sinrlib.UniformModel(config, N, S, 1 - 6*e)
-                model = sinrlib.SocialModel(config, N, S, e, 1 - 6*e, 0.1)
-                simulation = sinrlib.Simulation(model, lambda: sinrlib.ConstNoise(1.0))
-                algorithm = algorithms.DensityAlgorithm(config, e, C)
-                results.append(simulation.run(algorithm))
-                sys.stdout.write('.')
-                sys.stdout.flush()
-            except sinrlib.AlgorithmFailed:
-                print >> sys.stderr, 'algorithm failed!'
-                print 'empty rounds:', simulation.empty_rounds
-                print 'failed:', model.failed_transmit
-                print 'success:', model.success_transmit
-                for uid in algorithm.nodes:
-                    if not uid in algorithm.active:
-                        print >> sys.stderr, model.nodes[uid], 'is inactive'
-                #model.show()
+        for S in range(1, 7):
+            results = []
+            diams = []
+            while len(results) < tries:
+                try:
+                    model = sinrlib.UniformModel(config, N, S)
+                    #model = sinrlib.SocialModel(config, N, S, e, 0.1)
+                    simulation = sinrlib.Simulation(model, lambda: sinrlib.ConstNoise(1.0))
+                    #algorithm = algorithms.DensityUnknownAlgorithm(config, e, C)
+                    #algorithm = algorithms.DensityKnownAlgorithm(config, e, C, d)
+                    algorithm = algorithms.BackoffAlgorithm(config)
+                    results.append(simulation.run(algorithm, 1000))
+                    diams.append(model.diameter())
+                    sys.stdout.write('.')
+                    sys.stdout.flush()
+                except sinrlib.AlgorithmFailed:
+                    print >> sys.stderr, 'algorithm failed!'
+                    print 'empty rounds:', simulation.empty_rounds
+                    print 'failed:', model.failed_transmit
+                    print 'success:', model.success_transmit
+                    for uid in algorithm.nodes:
+                        print >> sys.stderr, model.nodes[uid], 'ppb =', algorithm.ppb[uid]
+                        if not uid in algorithm.active:
+                            print >> sys.stderr, model.nodes[uid], 'is inactive'
+                    model.show()
 
-        avg = float(sum(results)) / len(results)
-        std = math.sqrt(sum([(r - avg)**2 for r in results]) / len(results))
+            avg = float(sum(results)) / len(results)
+            std = math.sqrt(sum([(r - avg)**2 for r in results]) / len(results))
+            avg_diam = float(sum(diams)) / len(diams)
 
-        print
-        print N, avg, std
+            print
+            print N, d, S, avg, std, avg_diam
 
-        ns.append(N)
-        avgs.append(avg)
-        stds.append(std)
+            ns.append(N)
+            avgs.append(avg)
+            stds.append(std)
 
-    pyplot.cla()
-    pyplot.xlabel('Network size')
-    pyplot.ylabel('Avg rounds number')
-    pyplot.xlim(start - step, end + step)
-    pyplot.title('Results with e = %f, S = %d, C = %f' % (e, S, C))
-    pyplot.errorbar(ns, avgs, stds, ecolor = 'r')
-    pyplot.show()
 
 if __name__ == "__main__":
     import cProfile
