d6ca4b826e4542b9378fa878a0a45fc349ae5c2b
Tomasz Maciejewski <ponton@jabster.pl>
2012-07-27 08:47:20 +0200

    random.choose -> random.choice

--- sinrlib/models/social.py
+++ sinrlib/models/social.py
@@ -103,5 +103,5 @@ class SocialModel(model.Model):
                     if len(comp) >= n:
                         self.nodes = {uid : self.nodes[uid] for uid in comp}
                         self.links = {uid : self.links[uid].intersection(comp) for uid in comp}
-                        self.source = random.choose(self.nodes.keys())
+                        self.source = random.choice(self.nodes.keys())
                         return
